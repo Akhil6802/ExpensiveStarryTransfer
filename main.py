fe6acf51640b49b94a26dce991ceb57618e188a1
marupakaakhil68 <33540983-marupakaakhil68@users.noreply.replit.com>
2024-04-19 13:37:44 +0000

    Initial commit

---
+++ main.py
@@ -0,0 +1,15 @@
+import random
+import string
+chars = " " + string.punctuation + string.digits + string.ascii_letters
+chars = list(chars)
+key = chars.copy()
+random.shuffle(key)
+#print(f"chars:{chars}")
+#print(f"key:{key}")
+plain_text = input("Enter a message to encrypt:")
+chiper_text = ""
+for letter in plain_text:
+  index = chars.index(letter)
+  chiper_text += key[index]
+print(f" original message: {plain_text}")
+print(f" original message: {chiper_text}")
